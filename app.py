544833d0c9e6f33f4d40ee8897fbcf940bfdcb3d
Philip Rosen <philipjrosen@gmail.com>
2015-04-12 13:03:02 -0700

    install pyStatParser; parse arbitrary sentence sent in querystring and return as json response for visualization

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_file, request, jsonify
 import nltk
 from nltk import Tree
+from stat_parser import Parser
 import sys
 import json
 
@@ -10,36 +11,21 @@ app = Flask(__name__)
 def index():
     return send_file("static/index.html")
 
-groucho_grammar = nltk.CFG.fromstring("""
-    S -> NP VP
-    PP -> P NP
-    NP -> Det N | Det N PP | 'I'
-    VP -> V NP | VP PP
-    Det -> 'an' | 'my'
-    N -> 'elephant' | 'pajamas'
-    V -> 'shot'
-    P -> 'in'
-""")
+parser = Parser()
 
-sent = ['I', 'shot', 'an', 'elephant', 'in', 'my', 'pajamas']
-parser = nltk.ChartParser(groucho_grammar)
-
-trees = []
-
-for tree in parser.parse(sent):
-    trees.append(tree)
-
+#converts an nltk Tree to a dictionary
 def tree2dict(tree):
     return {tree.label(): [tree2dict(t)  if isinstance(t, Tree) else t
                         for t in tree]}
-result = tree2dict(trees[0])
-json.dump(result, sys.stdout, indent=2)
-
 
 @app.route('/parsetree')
 def parse_tree():
-    # sentence = request.args.get('sentence')
-    return jsonify(sentence=result)
+    sentence = request.args.get('sentence')
+    tree = parser.parse(sentence)
+    d = tree2dict(tree)
+    #log to the console for testing
+    json.dump(d, sys.stdout, indent=2)
+    return jsonify(sentence=d)
 
 if __name__ == '__main__':
     app.run(debug=True)
