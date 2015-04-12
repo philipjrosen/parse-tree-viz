510c07e0685c6a6443fd33fe0d2f6f0db992e0cf
Philip Rosen <philipjrosen@gmail.com>
2015-04-12 09:37:53 -0700

    rename directory structure

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return send_file("client/app/index.html")
+    return send_file("static/index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
