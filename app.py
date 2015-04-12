827776cbaa02b6726abfe21a2b35230d8879b508
Philip Rosen <philipjrosen@gmail.com>
2015-04-12 10:29:14 -0700

    handle get request with querystring

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request, jsonify
+import nltk
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return send_file("static/index.html")
+
+@app.route('/parsetree')
+def parse_tree():
+    sentence = request.args.get('sentence')
+    return jsonify(sentence=sentence)
 
 if __name__ == '__main__':
     app.run(debug=True)
