3674ab8baa5e79c4cdb76edcffac0af2be5df4bc
Philip Rosen <philipjrosen@gmail.com>
2015-04-12 20:10:56 -0700

    adds methods on controller and form in view for sending using input to server and rendering result with d3

--- static/app/home/homeCtrl.js
+++ static/app/home/homeCtrl.js
@@ -1,179 +1,89 @@
 angular.module('app.home', [])
 
-.controller('HomeCtrl', ['$scope', function ($scope) {
+.controller('HomeCtrl', ['$scope', 'parseFactory', function ($scope, parseFactory) {
   $scope.verb = 'Visualize';
   $scope.noun = 'this!';
   $scope.sentence = "";
-  var treeData = [
-    {
-      children: [{
-          children: [{
-              children: [{
-                  children: null,
-                  name: "the",
-                  parent: "DT"
-              }],
-              name: "DT",
-              parent: "NP"
-          }, {
-              children: [{
-                  children: null,
-                  name: "quick",
-                  parent: "NN"
-              }],
-              name: "NN",
-              parent: "NP"
-          }],
-          name: "NP",
-          parent: "NP+S"
-      }, {
-          children: [{
-              children: [{
-                  children: null,
-                  name: "brown",
-                  parent: "VB"
-              }],
-              name: "VB",
-              parent: "VP"
-          }, {
-              children: [{
-                  children: [{
-                      children: [{
-                          children: null,
-                          name: "fox",
-                          parent: "JJ"
-                      }],
-                      name: "JJ",
-                      parent: "NP"
-                  }, {
-                      children: [{
-                          children: null,
-                          name: "jumps",
-                          parent: "NN"
-                      }],
-                      name: "NN",
-                      parent: "NP"
-                  }],
-                  name: "NP",
-                  parent: "NP"
-              }, {
-                  children: [{
-                      children: [{
-                          children: null,
-                          name: "over",
-                          parent: "IN"
-                      }],
-                      name: "IN",
-                      parent: "PP"
-                  }, {
-                      children: [{
-                          children: [{
-                              children: null,
-                              name: "the",
-                              parent: "DT"
-                          }],
-                          name: "DT",
-                          parent: "NP"
-                      }, {
-                          children: [{
-                              children: null,
-                              name: "lazy",
-                              parent: "JJ"
-                          }],
-                          name: "JJ",
-                          parent: "NP"
-                      }, {
-                          children: [{
-                              children: null,
-                              name: "dog",
-                              parent: "NN"
-                          }],
-                          name: "NN",
-                          parent: "NP"
-                      }],
-                      name: "NP",
-                      parent: "PP"
-                  }],
-                  name: "PP",
-                  parent: "NP"
-              }],
-              name: "NP",
-              parent: "VP"
-          }],
-          name: "VP",
-          parent: "NP+S"
-      }],
-      name: "NP+S",
-      parent: null
-    }
-  ];
+  var treeData = [];
 
-  var margin = {top: 20, right: 120, bottom: 20, left: 120},
-    width = 960 - margin.right - margin.left,
-    height = 800 - margin.top - margin.bottom;
+  $scope.getSentence = function () {
+    console.log($scope.sentence);
+    $scope.getParsedTree($scope.sentence);
+    $scope.sentence = "";
+  };
 
-  var i = 0;
+  $scope.getParsedTree = function(sentence) {
+    parseFactory.getTree(sentence)
+    .then(function (parsedTree) {
+      treeData.push( parsedTree.sentence);
+      console.log(treeData);
+      var margin = {top: 20, right: 120, bottom: 20, left: 120},
+        width = 960 - margin.right - margin.left,
+        height = 800 - margin.top - margin.bottom;
 
-  //define the tree layout and get a reference to it
-  var tree = d3.layout.tree()
-    .size([height, width]);
+      var i = 0;
 
-  //declare the function to be used for connecting nodes and get a reference
-  var diagonal = d3.svg.diagonal()
-    .projection(function(d) { return [d.x, d.y]; });
+      //define the tree layout and get a reference to it
+      var tree = d3.layout.tree()
+        .size([height, width]);
 
-  //append svg element to body and define it's properties
-  var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      //declare the function to be used for connecting nodes and get a reference
+      var diagonal = d3.svg.diagonal()
+        .projection(function(d) { return [d.x, d.y]; });
 
-  root = treeData[0];
+      //append svg element to body and define it's properties
+      var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.right + margin.left)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  update(root);
+      root = treeData[0];
+      console.log("root", root);
+      update(root);
 
-  function update(source) {
+      function update(source) {
 
-  // Compute the new tree layout.
-  var nodes = tree.nodes(root).reverse();
-  var links = tree.links(nodes);
+      // Compute the new tree layout.
+      var nodes = tree.nodes(root).reverse();
+      var links = tree.links(nodes);
 
-  // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 100; });
+      // Normalize for fixed-depth.
+      nodes.forEach(function(d) { d.y = d.depth * 100; });
 
-  // Declare the node function/variable
-  var node = svg.selectAll("g.node")
-    .data(nodes, function(d) { return d.id || (d.id = ++i); });
+      // Declare the node function/variable
+      var node = svg.selectAll("g.node")
+        .data(nodes, function(d) { return d.id || (d.id = ++i); });
 
-  // Enter the nodes.
-  var nodeEnter = node.enter().append("g")
-    .attr("class", "node")
-    .attr("transform", function(d) {
-      return "translate(" + d.x + "," + d.y + ")"; });
+      // Enter the nodes.
+      var nodeEnter = node.enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) {
+          return "translate(" + d.x + "," + d.y + ")"; });
 
-  nodeEnter.append("circle")
-    .attr("r", 10)
-    .style("fill", "#fff");
+      nodeEnter.append("circle")
+        .attr("r", 10)
+        .style("fill", "#fff");
 
-  nodeEnter.append("text")
-    .style("fill", "#fff")
-    .attr("x", function(d) {
-      return d.children || d._children ? -13 : 13; })
-    .attr("dy", ".35em")
-    .attr("text-anchor", function(d) {
-      return d.children || d._children ? "end" : "start"; })
-    .text(function(d) { return d.name; })
-    .style("fill-opacity", 1);
+      nodeEnter.append("text")
+        .style("fill", "#fff")
+        .attr("x", function(d) {
+          return d.children || d._children ? -13 : 13; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", function(d) {
+          return d.children || d._children ? "end" : "start"; })
+        .text(function(d) { return d.name; })
+        .style("fill-opacity", 1);
 
-  // Declare the links
-  var link = svg.selectAll("path.link")
-    .data(links, function(d) { return d.target.id; });
+      // Declare the links
+      var link = svg.selectAll("path.link")
+        .data(links, function(d) { return d.target.id; });
 
-  // Enter the links.
-  link.enter().insert("path", "g")
-    .attr("class", "link")
-    .attr("d", diagonal);
-  }
-
+      // Enter the links.
+      link.enter().insert("path", "g")
+        .attr("class", "link")
+        .attr("d", diagonal);
+      }
+    });
+  };
 }]);
